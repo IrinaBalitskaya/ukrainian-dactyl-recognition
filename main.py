3b45fbc8f8dffe1b85b16f58ad22233a288c61a3
Irina Balitskaya <iryna@adozi.com>
2022-09-28 13:48:51 +0300

    init git

---
+++ main.py
@@ -0,0 +1,7 @@
+from opencv import capture_webcam_feed
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    capture_webcam_feed()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
