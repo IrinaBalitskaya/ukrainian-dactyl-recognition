22d7859230b9c597e24b52579e4e1e527b5f1aa3
Irina Balitskaya <iryna@adozi.com>
2022-10-27 01:18:50 +0300

    Add file main js with ajax functions

---
+++ static/js/main.js
@@ -0,0 +1,38 @@
+$(function(){
+    window.setInterval(function(){
+        updateTranslation()
+    }, 1000)
+
+   function updateTranslation(){
+     $.ajax({
+        url: "/translation",
+        type: "GET",
+        dataType: "json",
+        success: function(data){
+            $(translation_textarea).replaceWith(data)
+        }
+     });
+   }
+});
+
+$(document).on('click','#button_clear',function(){
+     $.ajax({
+        url: "/clear_textarea",
+        type: "DELETE"
+     });
+});
+
+$(document).on('click','#button_start',function(){
+     $.ajax({
+        url: "/start_pause_webcam",
+        type: "PUT",
+        contentType: "application/json",
+        dataType: 'json',
+        data: JSON.stringify({
+            btn_text: $(this).text()
+        }),
+        success: function(response){
+            $("#button_start").text(response.btn_text)
+        }
+     });
+});
