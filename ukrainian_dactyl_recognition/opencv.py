07e9ca80588b7b770a8450ccab359bb5abcef0e0
Irina Balitskaya <iryna@adozi.com>
2022-10-27 01:16:42 +0300

    Add function gesture_detection

---
+++ ukrainian_dactyl_recognition/opencv.py
@@ -0,0 +1,19 @@
+from cv2 import cv2
+
+
+def gesture_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
+
+
+##############
+
+
+
+
+
+
