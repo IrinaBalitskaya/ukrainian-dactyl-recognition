ee6885c6d1f683378354798e7ea3a9d64dbeb3fd
Irina Balitskaya <iryna@adozi.com>
2022-10-30 01:49:19 +0300

    Change flask app to application

--- application.py
+++ application.py
@@ -3,33 +3,33 @@ from ukrainian_dactyl_recognition.dactyl_detection import detect_dactyl
 from flask import Flask, render_template, Response, request, jsonify
 import ukrainian_dactyl_recognition.cfg as cfg
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/stream')
+@application.route('/stream')
 def stream():
     return Response(detect_dactyl(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-@app.route('/translation')
+@application.route('/translation')
 def translation():
     return jsonify('', render_template('translation_textarea.html',
                                        translation=''.join(cfg.sentence)))
 
 
-@app.route('/clear_textarea', methods=['DELETE'])
+@application.route('/clear_textarea', methods=['DELETE'])
 def clear_textarea():
     cfg.sentence = []
     return jsonify('', render_template('translation_textarea.html', translation=''))
 
 
-@app.route('/start_pause_webcam', methods=['PUT'])
+@application.route('/start_pause_webcam', methods=['PUT'])
 def start_pause_webcam():
     cfg.is_feed_on = not cfg.is_feed_on
     btn_text = request.get_json()['btn_text']
@@ -38,4 +38,4 @@ def start_pause_webcam():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
